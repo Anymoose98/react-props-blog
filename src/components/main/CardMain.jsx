78553404d88e9dc8b0f3e707096cdeb50a72f8f0
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-06-13 16:35:58 +0200

    Mostra solo quelli pubblished e usa le prop con Responsive

---
+++ src/components/main/CardMain.jsx
@@ -0,0 +1,28 @@
+import mainStyles from './Main.module.scss'
+
+const Card = ({ id, title, image, content, tags, published }) => {
+    return (
+        <div className={`Card ${published ? 'col-12 col-md-6 col-lg-3' : mainStyles.nascondi} `}>
+            <div class="">
+                <div className={mainStyles.card}>
+                    <figure>
+                        <img src={image} alt={title} className={mainStyles.img} />
+                    </figure>
+                    <div className={mainStyles.bgWhite}>
+                        <h3>{title}</h3>
+                        <p>{content}</p>
+                        <h5>
+                            {tags.map((tags, index) => (
+                                <strong key={index}>{tags} </strong>
+                            ))}
+                        </h5>
+                        <button className={mainStyles.bottone}>Leggi di più</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
+export default Card
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,17 +1,84 @@
 import mainStyles from './Main.module.scss'
-import foto from '../../assets/snowy-mountain-peak-starry-galaxy-majesty-generative-ai.jpg'
+import foto1 from '../../assets/snowy-mountain-peak-starry-galaxy-majesty-generative-ai.jpg'
+import foto2 from '../../assets/beautiful-tropical-empty-beach-sea-ocean-with-white-cloud-blue-sky-background.jpg'
+import foto3 from '../../assets/field-surrounded-by-rocks-covered-greenery-blue-sky-sunlight-italy.jpg'
+import foto4 from '../../assets/pretty-cloudscape.jpg'
+import noFoto from '../../assets/6605525.jpg'
+import Card from './CardMain.jsx'
+const posts = [
+    {
+      id: 1,
+      title: "Universo e Terra ",
+      image: `${foto1}`,
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["Fantasy"],
+      published: true,
+    },
+    {
+      id: 2,
+      title: "Tutti al mare",
+      image: `${foto2}`,
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["Estate", "Mare"],
+      published: true,
+    },
+    {
+      id: 3,
+      title: "Montagna e verde",
+      image: `${foto3}`,
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["Montagna", "Natura"],
+      published: true,
+    },
+    {
+      id: 4,
+      title: "Il blu del cielo",
+      image: `${foto4}`,
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["Aria pulita"],
+      published: false,
+    },
+    {
+        id: 5,
+        title: "Foto casuali",
+        image: `https://picsum.photos/300`,
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+        tags: ["Casuale", "Random"],
+        published: false,
+      },
+      {
+        id: 5,
+        title: "Senza foto",
+        image: `${noFoto}`,
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+        tags: ["Trapezio"],
+        published: true,
+      },
+    
+  ];
 
 const Main = () => {
     return (
         <main className={mainStyles.bgMain}>
-            <div className={mainStyles.card} >
-                <figure>
-                    <img src={foto} alt="foto-spazio" className={mainStyles.img} />
-                </figure>
-                <div classname={mainStyles.bgWhite}>
-                    <h3>Dove universo e terra si incontrano</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <button className={mainStyles.bottone}>Leggi di più</button>
+            <div class="container">
+                <div class="row">
+                    {posts.map((p, index) => (
+                            <Card 
+                                key={`${index}`}
+                                id={p.id}
+                                title={p.title}
+                                image={p.image}
+                                content={p.content}
+                                tags={p.tags}
+                                published={p.published}
+                            />
+                    ))}
                 </div>
             </div>
         </main>
